35c14f32b1ce8fa1ba584ea5a18201f068c58133
hman0 <108770550+hman0@users.noreply.github.com>
2022-08-06 01:12:22 +0100

    Add files via upload

---
+++ iceclub spam.lua
@@ -0,0 +1,64 @@
+function IceClub()
+    game.ReplicatedStorage.GiveSword:InvokeServer("IceClub", game.Players.LocalPlayer)
+end
+
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
+spawn(function()
+    IceClub() 
+    iceclub_count = iceclub_count + 1
+end
